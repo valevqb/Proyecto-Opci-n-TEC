4259d4379d2f10c1397c7ed30b5cd5b66785dce2
AM4111 <pauma1018@gmail.com>
2022-08-16 01:23:18 -0600

    .
    .

---
+++ Proyecto/Practica/lib/Barra.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'Pez1_Screen.dart';
+
+class PrototipoBarra extends StatefulWidget {
+  State<StatefulWidget> createState() => _PrototipoBarra();
+}
+
+
+class _PrototipoBarra extends State<PrototipoBarra> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Plantilla(),
+    Plantilla2(),
+    PezColumn(),
+    PezText(),
+    PezPading(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pez Prueba",
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Pez pantalla 2"),
+        ),
+        body: _children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: onTabTapped,
+          selectedItemColor: Colors.cyan,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: "Inicio"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt), label: "Admision"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.school), label: "Carreras"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.synagogue), label: "Campus"
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.dehaze_rounded), label: "Mas"
+            ),
+          ],
+        ),
+      ), //),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
---
+++ Proyecto/Practica/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+//import 'Pez1_Screen.dart';
+import 'Barra.dart';
+
+void main() {
+  runApp(PrototipoBarra());
+}
