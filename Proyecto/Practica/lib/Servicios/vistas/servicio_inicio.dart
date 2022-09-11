de3429786d90ad1ded5a93c738d609e694516ddc
Valeria Quesada <vale.vqb03@gmail.com>
2022-09-11 13:31:23 -0600

    Servicios

--- Proyecto/Practica/lib/Servicios/vistas/servicio_inicio.dart
+++ Proyecto/Practica/lib/Servicios/vistas/servicio_inicio.dart
@@ -23,23 +23,147 @@ class _InicioServicioState extends State<InicioServicio> {
     List<DataServicio>? users = Provider.of<DatosServicio>(context).servicios;
     bool isLoading = Provider.of<DatosServicio>(context).isLoading;
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: Text('Servicios'),
         elevation: 0,
         backgroundColor: Colors.red,
-        toolbarHeight: 286,
-      ),
+        toolbarHeight: ,
+      ),*/
       body: (isLoading)
           ? Container(
+
         )
           : Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                topRight: Radius.circular(24)
-              ),
-            ),
             child: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 286,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0m5aY2oIKILiSLtlWgKyaHLDfgqqXVuRtnhkiLrXT_5rmM7Chm8XBjeKbwC1fcOeexuU&usqp=CAU'),
+                          )
+                        ),
+                      ),
+                      SizedBox(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                          ),
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: users!.length,
+                              itemBuilder: (BuildContext context, int index){
+                                return Align(
+                                  alignment: Alignment.topLeft,
+                                    child: Container(
+                                      height: 208,
+                                      margin: EdgeInsets.only(left: 21.4, top: 32),
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Container(
+                                            margin: const EdgeInsets.only(bottom: 16.0),
+                                            child: Text(
+                                            users[index].firstName!,
+                                            style: TextStyle(
+                                            fontFamily: 'Poppins',
+                                            fontSize: 18,
+                                            fontWeight: FontWeight. bold,
+                                            color: Color(0xFFB2B436D))
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            height: 168,
+                                            child: ListView.builder(
+                                              scrollDirection: Axis.horizontal,
+                                              itemCount: users!.length,
+                                              itemBuilder: (BuildContext context, int index) {
+                                                return Align(
+                                                  alignment: Alignment.topLeft,
+                                                  child: Container(
+                                                    width: 151,
+                                                    height: 168,
+                                                    child: Card(
+                                                      semanticContainer: true,
+                                                      shape: RoundedRectangleBorder(
+                                                        borderRadius: BorderRadius.circular(24),
+                                                      ),
+                                                      child: new InkWell(
+                                                        onTap: () {
+                                                          print("tapped " +  users[index].firstName!);
+                                                          },
+                                                        child: Container(
+                                                          child: Row(
+                                                            children: [
+                                                              Column(
+                                                                mainAxisAlignment: MainAxisAlignment.start,
+                                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                                children: [
+                                                                  ClipRRect(
+                                                                    borderRadius: BorderRadius.only(
+                                                                      topLeft: Radius.circular(20),
+                                                                      topRight: Radius.circular(20),
+                                                                    ),
+                                                                    child: Image.network( // Imagen principal
+                                                                      users[index].avatar!,
+                                                                      fit: BoxFit.fill,
+                                                                      width: 143,
+                                                                      height: 90,
+                                                                    ),
+                                                                  ),
+                                                                  Container(
+                                                                      margin: const EdgeInsets.only(top: 8.0, left: 16),
+                                                                      child: CircleAvatar (
+                                                                        radius: 16,
+                                                                        backgroundImage: NetworkImage(
+                                                                          users[index].avatar!,
+                                                                        ),
+                                                                      )
+                                                                  ),
+                                                                  Container(
+                                                                    margin: const EdgeInsets.only(top: 8.0, left: 16),
+                                                                    child: Text (
+                                                                      users[index].firstName!,
+                                                                      style: TextStyle(fontFamily: 'Mulish',
+                                                                          fontSize: 14, fontWeight: FontWeight. bold),
+                                                                    ),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                            ],
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                );
+                                              }
+                                            )
+                                          )
+                                        ],
+                                      )
+                                    ),
+                                );
+                              }
+                          ),
+                        ),
+                      )
+                    ]
+                  ),
+                ),
+              );
+            }
+          )
+            /*child: ListView.builder(
             itemCount: users!.length,
             itemBuilder: (BuildContext context, int index) {
               return Align(
@@ -50,7 +174,7 @@ class _InicioServicioState extends State<InicioServicio> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
+                    children: [ ///////////
                       Container(
                         margin: const EdgeInsets.only(bottom: 16.0),
                         child: Text(
@@ -69,7 +193,7 @@ class _InicioServicioState extends State<InicioServicio> {
                               scrollDirection: Axis.horizontal,
                               itemCount: users!.length,
                               itemBuilder: (BuildContext context, int index) {
-                                return Align(
+                                return Align( /////////
                                   alignment: Alignment.topLeft,
                                   child: Container(
                                       width: 151,
@@ -79,7 +203,11 @@ class _InicioServicioState extends State<InicioServicio> {
                                           shape: RoundedRectangleBorder(
                                             borderRadius: BorderRadius.circular(24),
                                           ),
-                                          child: Container(
+                                        child: new InkWell(
+                                            onTap: () {
+                                              print("tapped " +  users[index].firstName!);
+                                            },
+                                          child: Container( //////
                                             child: Row(
                                               children: [
                                                 Column(
@@ -97,7 +225,7 @@ class _InicioServicioState extends State<InicioServicio> {
                                                         width: 143,
                                                         height: 90,
                                                       ),
-                                                    ),
+                                                    ), ----------------------------------------------
                                                     Container(
                                                         margin: const EdgeInsets.only(top: 8.0, left: 16),
                                                         child: CircleAvatar (
@@ -119,136 +247,19 @@ class _InicioServicioState extends State<InicioServicio> {
                                                 ),
                                               ],
                                             ),
-                                          )
-
-                                      )
+                                          ),
+                                      ),
+                                    ),
                                   ),
                                 );
                               }),
                       ),
-                      /*Container(
-                          width: 151,
-                          height: 168,
-                          child: Card(
-                              semanticContainer: true,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24),
-                              ),
-                              child: Container(
-                                child: Row(
-                                  children: [
-                                    Column(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(20),
-                                            topRight: Radius.circular(20),
-                                          ),
-                                          child: Image.network( //No quiere redimensionarse
-                                            users[index].avatar!,
-                                            fit: BoxFit.fill,
-                                            width: 143,
-                                            height: 90,
-                                          ),
-                                        ),
-                                        Container(
-                                            margin: const EdgeInsets.only(top: 8.0, left: 16),
-                                            child: CircleAvatar (
-                                              radius: 16,
-                                              backgroundImage: NetworkImage(
-                                                users[index].avatar!,
-                                              ),
-                                            )
-                                        ),
-                                        Container(
-                                          margin: const EdgeInsets.only(top: 8.0, left: 16),
-                                          child: Text (
-                                            users[index].firstName!,
-                                            style: TextStyle(fontFamily: 'Mulish',
-                                                fontSize: 14, fontWeight: FontWeight. bold),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              )
-
-                          )
-                      ),*/
                     ],
                   ),
                 ),
-                /*child: Container(
-                    width: 151,
-                    height: 168,
-                    child: Card(
-                        semanticContainer: true,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        ),
-                        child: Container(
-                          child: Row(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(20),
-                                      topRight: Radius.circular(20),
-                                    ),
-                                    child: Image.network( //No quiere redimensionarse
-                                        users[index].avatar!,
-                                        fit: BoxFit.fill,
-                                        width: 143,
-                                        height: 90,
-                                    ),
-                                  ),
-                                  Container(
-                                      margin: const EdgeInsets.only(top: 8.0, left: 16),
-                                      child: CircleAvatar (
-                                        radius: 16,
-                                        backgroundImage: NetworkImage(
-                                        users[index].avatar!,
-                                        ),
-                                      )
-                                  ),
-                                  Container(
-                                      margin: const EdgeInsets.only(top: 8.0, left: 16),
-                                      child: Text (
-                                        users[index].firstName!,
-                                        style: TextStyle(fontFamily: 'Mulish',
-                                        fontSize: 14, fontWeight: FontWeight. bold),
-                                      ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        )
-
-                    )
-                ),*/
               );
-            })
+            })*/
       )
     );
   }
 }
-     /*title: Text(
-                  users[index].firstName! + " " + users[index].lastName!,
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87),
-                ),
-                subtitle: Text(
-                  users[index].email!,
-                  style: TextStyle(fontSize: 16, color: Colors.black54),
-                ),
-                trailing: Text('ID: ' + users[index].id.toString()),*/
-
