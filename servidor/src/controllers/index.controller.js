98705e57b748e8d8ec5763907bc3ea04bd94aa5c
javith20 <43230203+javith20@users.noreply.github.com>
2022-09-21 00:58:12 -0600

    server

---
+++ servidor/src/controllers/index.controller.js
@@ -0,0 +1,61 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    host: '192.168.18.13',
+    user: 'opcionTec',
+    password: '2003',
+    database: 'OpcionTEC',
+    port: '5432'
+})
+
+const getCarreras =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM Carreras');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+const getPreguntas =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM Preguntas');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+const getBecas =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM Becas');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+const getServicios =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM servicios');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+const getCostes =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM costes');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+const getBus =  async (req,res) => {
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * FROM Servicios_Bus');
+    newJson["Datos"]=response.rows
+    res.json(newJson)
+}
+
+
+module.exports = {
+    getPreguntas,
+    getCarreras,
+    getBecas,
+    getServicios,
+    getCostes,
+    getBus
+}
