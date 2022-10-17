fa09aecc7c172b8ba85435a25863083f619024ef
AM4111 <aguerojavith@gmail.com>
2022-10-17 15:20:40 -0600

    Update eventos.controller.js

--- servidor/src/controllers/eventos.controller.js
+++ servidor/src/controllers/eventos.controller.js
@@ -1,10 +1,11 @@
 const {pool} = require('../database/postgeSQL');
 
 const getEventos =  async (req,res) => {
-   
-    const response =  await pool.query('SELECT getEventos()');
-    console.debug(response.rows[0]);
-    res.json(JSON.parse(response.rows[0]))
+    var newJson = {"Cantidad":2,"Datos":[{}]}
+    const response =  await pool.query('SELECT * from eventos');
+    newJson["Datos"]=response.rows
+    console.debug(response.rows);
+    res.json(newJson)
 }
 
 module.exports = {getEventos};
